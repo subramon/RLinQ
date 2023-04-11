87083f02c5a1e8e8eab2c598bbb25834b9044e50
Ramesh Subramonian <subramonian@gmail.com>
2023-04-11 08:03:33 +0100

    ok

--- 3_2/3_2_process_2.lua
+++ 3_2/3_2_process_2.lua
@@ -3,16 +3,22 @@ local T = require 'inputs_3_2'
 
 local state = {}
 state.Xt = T.X0 -- initial value 
+state.Xt_minusX_t_minus_1 = nil
 
 for i = 1, T.time_steps do 
-  local p = 1.0/(1 + math.exp(-1.0 * T.alpha1 * (T.L - state.Xt)))
-  assert(p >= 0)
-  local coin_toss = utils.coin_toss_rv(p)
+  state.Xt_minusX_t_minus_1 = state.Xt
+  local p
+  if ( i == 1 ) then 
+    p = 0.5
+  else
+    p = 0.5 * (1 - (T.alpha2 * (state.Xt_minusX_t_minus_1)))
+  end
+  local coin_toss = utils.coin_toss_rv(0.5)
   if ( coin_toss == "heads" ) then 
     state.Xt = state.Xt - 1
   else
     state.Xt = state.Xt + 1 
   end
-  print(state.Xt)
+ print(state.Xt)
 end
 return state
--- 3_2/inputs_3_2.lua
+++ 3_2/inputs_3_2.lua
@@ -4,4 +4,5 @@ T.num_traces = 10  -- number of traces
 T.time_steps = 1000  -- number of time steps
 T.L  = 100 -- arbitrary reference level
 T.alpha1 = 0.25 -- "pull strength" parameter
+T.alpha2 = 0.75 -- "pull strength" parameter
 return T
